914ea65cfcd8832b3be8044b81770c9b2144dc68
Francesc <francesc@gmail.com>
2018-03-21 23:13:17 +0100

    chore: adjust image sizes

--- js/main.js
+++ js/main.js
@@ -137,14 +137,16 @@ const imgSizes = [340, 400, 600, 800];
  * Create restaurant HTML.
  */
 createRestaurantHTML = (restaurant) => {
+  console.log(window.innerWidth)
   const fileName = DBHelper.imageFileForRestaurant(restaurant);
-  const srcset = imgSizes.map(size => `/img-${size}/${fileName} ${size}w`).join(' ')
+  const srcset = imgSizes.map(size => `/img-${size}/${fileName} ${size}w`).join(', ')
+  const sizes = imgSizes.map(size => `(max-width:${size + 20}px) ${size}px`).join(', ') + ', 800px'
 
   const li = document.createElement('li');
   const image = document.createElement('img');
   image.setAttribute('alt', 'Restaurant picture');
+  image.setAttribute('sizes', sizes);
   image.setAttribute('srcset', srcset);
-  image.setAttribute('sizes', '100vw');
   image.className = 'restaurant-img';
   //image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.src = `/img-340/${fileName}`;
