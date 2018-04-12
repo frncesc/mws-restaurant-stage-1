208b97af74a2f064c4e8741d40e862bbc4043d92
Francesc Busquets <francesc@gmail.com>
2018-04-12 11:37:42 +0200

    feat: Re-organize directories and use Gulp
    - Project files are now organized in five directories:
      - `media`: Contains the original pictures of the restaurants, and a big logo.
      - `src`: Contains the source files (.html, .css, .js, .json...) and also auto-generated images in different sizes and formats.
      - `dist`: This folder will be generated by Gulp, and will contain the final optimized files to be deployed in production.
      - `node_modules`: Auto-generated by `npm`, contains plugins and needed components.
    
      The root folder contains files related to package and workflow control: readme, changelog, gulpfile...
    
    - A Gulp script has been created, providing the following features:
      - Auto generation of pictures and logos in different sizes and formats (JPEG, PNG and WEBP). The script `resize-images.js` is no longer needed and has been removed.
      - Copy and optimize files in `src` and `dist`
      - Launch a live server, both in devel (src) and dist modes, watching for changes.
    
    - The following npm commands can be used to compile and launch the development and distribution environments:
      - `npm run debug`
      - `npm run serve`

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+
+// Load Gulp extensions
 const gulp = require('gulp');
 const del = require('del');
 const newer = require('gulp-newer');
@@ -5,41 +7,54 @@ const responsive = require('gulp-responsive');
 const liveServer = require('live-server');
 const runSequence = require('run-sequence');
 
-const LOGO_SIZES = [512, 192, 144, 96, 72, 64, 48, 16];
+// Take the original files located in `media/pictures` and generate
+// optimized images in JPEG, PNG and WEBP formats, resized to the widths specified in PICTURE_SIZES
 const PICTURE_SIZES = [340, 400, 600, 800];
-
 gulp.task('build:pictures', () => {
   return gulp.src('media/pictures/*.{jpg,png}')
+    // Act only when the original media is newer than the auto-generated one
     .pipe(newer({
       dest: 'src/pictures',
       map: (fn) => fn.split('.')[0] + '-800px.' + fn.split('.')[1],
     }))
-    .pipe(responsive({'*': PICTURE_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}px` } }; })
-    .concat(PICTURE_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}px`, extname: '.webp' } }; }))},
-    { quality: 70,
-      progressive: true,
-      withMetadata: false,
-      errorOnUnusedConfig: false,
-      silent: true,
-    }))
+    .pipe(responsive(
+      {
+        '*': PICTURE_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}px` } }; })
+          .concat(PICTURE_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}px`, extname: '.webp' } }; }))
+      },
+      {
+        quality: 70,
+        progressive: true,
+        withMetadata: false,
+        errorOnUnusedConfig: false,
+        silent: true,
+      }))
     .pipe(gulp.dest('src/pictures'));
 });
 
+// Take the original file `media/logo/icon.png` and generate
+// variants resized to the widths specified in LOGO_SIZES
+const LOGO_SIZES = [512, 192, 144, 96, 72, 64, 48, 16];
 gulp.task('build:logo', () => {
   return gulp.src('media/logo/icon.png')
+    // Act only when the original media is newer than the auto-generated one
     .pipe(newer({
       dest: 'src/logo',
       ext: '-512x512.png'
     }))
     .pipe(responsive(
-      { '*': LOGO_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}x${s}` } }; }) },
-      { withMetadata: false,
+      {
+        '*': LOGO_SIZES.map(s => { return { width: s, rename: { suffix: `-${s}x${s}` } }; })
+      },
+      {
+        withMetadata: false,
         errorOnUnusedConfig: false,
         silent: true,
       }))
     .pipe(gulp.dest('src/logo'));
 });
 
+// Start a live server based on `src`
 gulp.task('serve:src', function () {
   liveServer.start({
     port: 8000,
@@ -51,15 +66,18 @@ gulp.task('serve:src', function () {
   });
 })
 
+// Watch for changes in `src`
 gulp.task('watch:src', function () {
   gulp.watch('media/pictures/*', ['build-pictures']);
   gulp.watch('media/logo/icon.png', ['build-logo']);
 })
 
+// Remove `dist`
 gulp.task('clean:dist', (done) => {
   return del(['dist'], done);
 });
 
+// Fill `dist` with the needed components
 gulp.task('copy:dist', function () {
   // Todo: minify js
   gulp.src('src/**/*.{html,js,css,json,jpg,png,webp}')
@@ -67,6 +85,7 @@ gulp.task('copy:dist', function () {
     .pipe(gulp.dest('dist'));
 });
 
+// Start a live server based on `dist`
 gulp.task('serve:dist', function () {
   liveServer.start({
     port: 8010,
@@ -78,16 +97,19 @@ gulp.task('serve:dist', function () {
   });
 })
 
+// Watch for changes in `dist`
 gulp.task('watch:dist', function () {
   gulp.watch(['src/**/*.{html,js,css,json}'], ['copy:dist']);
   gulp.watch('media/pictures/*', ['build:pictures', 'copy:dist']);
   gulp.watch('media/logo/icon.png', ['build:logo', 'copy:dist']);
 })
 
+// Prepare `dist`, launch the HTTP server, launch the main page in a browser and reload it when changes are detected
 gulp.task('serve', function (callback) {
   runSequence('clean:dist', ['build:pictures', 'build:logo'], 'copy:dist', ['serve:dist', 'watch:dist'], callback);
 });
 
+// Prepare `src` (unoptimized sources), launch the HTTP server, launch the main page in a browser and reload it when changes are detected
 gulp.task('debug', function (callback) {
   runSequence(['build:pictures', 'build:logo'], ['serve:src', 'watch:src'], callback);
 });
